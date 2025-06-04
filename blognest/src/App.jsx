2cf50af324bfa4bae34e5c93de47989dc8d2c5fc
Feroze Mohammad <writeto_feroze2002@yahoo.co.uk>
2025-06-04 10:13:37 +0530

    setting up configs

--- blognest/src/App.jsx
+++ blognest/src/App.jsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "../styles/App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  // useEffect(() => {
+  //   document.documentElement.classList.add("dark")
+  // }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+    <div className="min-h-screen bg-red-100 text-gray-900 dark:bg-gray-900 dark:text-white flex flex-col items-center justify-center">
+      <h1 className="text-[200px] font-bold mb-4">Tailwind is Working!</h1>
+      <p className="text-lg">
+        Try toggling the{" "}
+        <code className="bg-gray-200 px-1 py-0.5 rounded">dark</code> class on
+        the HTML tag.
       </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
